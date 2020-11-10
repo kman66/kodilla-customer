503716716765fff22c67d314d3c0790b0bd9b8fb
vls6 <mress@tutanota.com>
2020-11-10 18:16:52 +0100

    first commit

---
+++ src/main/java/com/kodilla/customer/KodillaCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaCustomerApplication.class, args);
+	}
+
+}
