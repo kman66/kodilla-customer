3d1f7ea77e9b0a1afd4cf1e35e317f83788ac0f1
kman66 <mress@tutanota.com>
2020-11-17 08:04:15 +0100

    use ribbon instead of eureka

--- src/main/java/com/kodilla/customer/connector/AccountsConnector.java
+++ src/main/java/com/kodilla/customer/connector/AccountsConnector.java
@@ -1,10 +1,13 @@
 package com.kodilla.customer.connector;
 
+import com.kodilla.customer.configuration.RibbonConfiguration;
 import com.kodilla.customer.mapper.GetAccountsResponse;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RibbonClient(name = "accounts", configuration = RibbonConfiguration.class)
 @FeignClient(name = "accounts")
 public interface AccountsConnector {
 	@GetMapping("/v1/accounts")
