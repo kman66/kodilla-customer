3640a2e39aacc3c107da491becc72e7a9c49c7d4
kman66 <mress@tutanota.com>
2020-11-19 20:34:01 +0100

    trying to configure gateway

--- src/main/java/com/kodilla/customer/KodillaCustomerApplication.java
+++ src/main/java/com/kodilla/customer/KodillaCustomerApplication.java
@@ -3,12 +3,13 @@ package com.kodilla.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableCircuitBreaker
 @EnableFeignClients
-//@EnableDiscoveryClient
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.netflix.client.config.IClientConfig", "com.kodilla.customer"})
 public class KodillaCustomerApplication {
 
 	public static void main(String[] args) {
